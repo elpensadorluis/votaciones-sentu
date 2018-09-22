c567fff3bcb09810913c541ea472c5b5b3265280
Luis Echenique <elpensadorluis@gmail.com>
2018-09-22 04:38:08 -0400

    dinámicas de botones para gráficas

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var myFirebaseRef;
 var chart;
-var chartData = [
-    ['candidato1', 50],
-    ['candidato2', 30],
-    ['candidato3', 144],
-    ['candidato4',1]
-];
+var chartData = [];
+// var chartData = [
+//     ['candidato1', 50],
+//     ['candidato2', 30],
+//     ['candidato3', 144],
+//     ['candidato4',1]
+// ];
 
 
 
@@ -28,15 +29,25 @@ $(document).ready(function () {
         // console.log('Los datos han cambiado', JSON.stringify(sanpshot.val(), null, 2));
         var candidatos = sanpshot.val();
         var votoTotal = 0;
+        chartData = [];
+        var arr;
         for (var candidato in candidatos) {
             // console.log(candidato, candidatos[candidato].votos, candidatos[candidato]);
             $("#votos_" + candidato + " span").text(candidatos[candidato].votos);
+            arr = [candidato, candidatos[candidato].votos];
+            chartData.push(arr);
             votoTotal += Number(candidatos[candidato].votos);
 
         }
         // console.log(votoTotal);
         $("#total span").html(votoTotal);
+        chart.load({
+            columns: chartData
+        });
     });
+    $("#bar, #pie, #donut").click(function () {
+        chart.transform(this.id);
+      })
 
     $("#candidato2, #candidato1, #candidato3, #candidato4").click(function () {
         // $("button[id^='candidato']").click(function () {
@@ -51,4 +62,46 @@ $(document).ready(function () {
         $("#" + choice).prop("disabled", false);
         console.log("votaste por " + choice + " y lleva " + voteCount);
     });
+
+    chart = c3.generate({
+        bindto: "#chart",
+        data: {
+            type: 'bar',
+            columns: chartData,
+            colors: {
+                candidato1: '#265a88',
+                candidato2: '#419641',
+                candidato3: '#2aabd2',
+                candidato4: '#eb9316'
+            },
+            names: {
+                candidato1: 'Candidato 1',
+                candidato2: 'Candidato 2',
+                candidato3: 'Candidato 3',
+                candidato4: 'Candidato 4'
+            }
+        },
+        bar: {
+            width: {
+                ratio: 1
+            }
+        },
+        tooltip: {
+            format: {
+                title: function(x) {
+                    return 'Estado de votación';
+                }
+            }
+        },
+        axis: {
+            rotated: true,
+            y: {
+                label: 'Cantidad de votos'
+            },
+            x: {
+                show: true,
+                label: 'Candidatos'
+            }
+        }
+    })
 });
